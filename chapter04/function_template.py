37586fc991f7d70f37416098a084a6bc6a8fc4e8
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-06 11:59:43 +0300

    finish chapter 4

---
+++ chapter04/function_template.py
@@ -0,0 +1,6 @@
+def a_descriptive_name(optional_argument):
+    """A documentation string"""
+    # Your function code goes here
+    # Your function code goes here
+    # Your function code goes here
+    return optional_argument
---
+++ chapter04/mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tool',
+    author='HF Python 2e',
+    email='hfpy2@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch']
+)
