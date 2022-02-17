536fa0cc882d2d35b43d72a31d49e95b391e0c0c
EdwardFalcon <devedwardfalcon@gmail.com>
2022-02-17 16:08:19 +0300

    finish chapter 8 and 9

---
+++ chapter09/test_db.py
@@ -0,0 +1,16 @@
+from DBcm import UseDatabase
+
+dbconfig = {
+    'host': '127.0.0.0',
+    'user': 'vsearch',
+    'password': '',
+    'database': 'vsearchlogDB',
+}
+
+with UseDatabase(dbconfig) as cursor:
+    _SQL = """show tables"""
+    cursor.execute(_SQL)
+    data = cursor.fetchall()
+
+
+print(data)
